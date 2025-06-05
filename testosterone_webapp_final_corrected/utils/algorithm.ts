5752907f81c73bc5d924d72eae143083dbb90d97
6lettersboss <sungjin.kim@6letters.co.kr>
2025-06-05 12:51:16 +0900

    Delete testosterone_webapp_final_corrected directory

--- testosterone_webapp_final_corrected/utils/algorithm.ts
+++
@@ -1,8 +0,0 @@
-
-export function calculateTestosterone(input: any): number {
-  let base = 500;
-  base += input.exercise ? 40 : 0;
-  base -= input.sleepApnea ? 20 : 0;
-  base += input.clomiphene ? 50 : 0;
-  return base;
-}
