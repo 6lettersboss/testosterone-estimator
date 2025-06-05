792de23ac440f040d21e5e345cd868d68ee7dbb6
6lettersboss <sungjin.kim@6letters.co.kr>
2025-06-05 13:01:25 +0900

    Delete testosterone-js-webapp directory

--- testosterone-js-webapp/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 50, textAlign: 'center' }}>
-      <h1>MOJO KING Testosterone Estimator</h1>
-      <p>Welcome to the JavaScript version of the estimator app.</p>
-    </main>
-  );
-}
